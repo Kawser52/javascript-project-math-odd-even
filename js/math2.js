9cbbee406bc65ecfd2826d29e0342b11e35c6c83
TechDev-CEO <shoriful521399@gmail.com>
2022-08-25 09:16:30 +0600

    Project Complete

--- js/math2.js
+++ js/math2.js
@@ -2,26 +2,24 @@ const form = document.getElementById('form')
 form.addEventListener('submit', (e)=>{
    e.preventDefault(); 
    let seperate = '';
-   let searchKeword = numberInput.value;
-   console.log(searchKeword)
-   if(searchKeword<0){
-    console.log("Please give postive number")
-   } else if(searchKeword % 2 == 0){
-      console.log('this is even number');
+   let inputValue = numberInput.value;
+   if(inputValue<0){
+    document.getElementById('resultNumber').innerText = 'Enter a positive number';
+   } else if(inputValue % 2 == 0){
        for(let i = 1; i<4; i++){
-         searchKeword = +searchKeword+2;
-         seperate+=searchKeword + ", ";
-         console.log(searchKeword);
+         inputValue = +inputValue + 2;
+         seperate+=inputValue + ", ";
+         // console.log(inputValue);
        }
-       document.getElementById('kita').innerText = seperate;
+       document.getElementById('resultNumber').innerText = seperate;
        
    } else{
-    console.log('this is odd number');
+   //  console.log('this is odd number');
       for(let i = 1; i<4; i++){
-         searchKeword = +searchKeword+2;
-         seperate+=searchKeword + ", ";
-         console.log(searchKeword);
+         inputValue = +inputValue+2;
+         seperate+=inputValue + ", ";
+         // console.log(inputValue);
       }
-      document.getElementById('kita').innerText = seperate;
+      document.getElementById('resultNumber').innerText = seperate;
    }
 })
